4700f597e22343dc5bc4dc7836b7280b666b63e0
Ryan <wes15034@byui.edu>
2018-05-02 07:20:49 -0600

    5/2/18 changes to set.js

--- AllEventsTicketing/Set.js
+++ AllEventsTicketing/Set.js
@@ -1,112 +1,121 @@
 function Set() {
-	
+
 //Intersection
-	this.intersection = function(listA, listB) {
-    
-	   var resultList = []; /* create a resultList array */
-       
-	  if (listA === null || listB === null) { /* check for valid inputs*/
-	  	return null; // exit and return null to indicate an error
-	  }
-       for (var i=0; i < listA.length; i++) {  //for every element in listA
-	  	var nextValue = listA[i]; // get next value in the list
+    this.intersection = function (listA, listB) {
 
-		for (var j = 0; j < listB.length; j++) { // for every element in listB
-		 if (listB[j] === nextValue) { // this listB element equals nextValue
-		 	resultList.push(listB[j]); // add listB element to end of resultList
-			 // push adds the current item in j to the end of the result list
-			 break;  // break the listB inner loop and exit
-
-		 }
-
-		}
-	   }
-	   return resultList;
-	}
-    
-    				// I used stack overflow and Khan Academy to figure this out.
-//Union
-
-	this.union = function(listA, listB) {
-
-	   var resultList = [];
+        var resultList = [];
+        /* create a resultList array */
 
         if (listA === null || listB === null) { /* check for valid inputs*/
             return null; // exit and return null to indicate an error
+        }
+        for (var i = 0; i < listA.length; i++) {  //for every element in listA
+            var nextValue = listA[i]; // get next value in the list
+
+            for (var j = 0; j < listB.length; j++) { // for every element in listB
+                if (listB[j] === nextValue) { // this listB element equals nextValue
+                    resultList.push(listB[j]); // add listB element to end of resultList
+                    // push adds the current item in j to the end of the result list
+                    break;  // break the listB inner loop and exit
+
+                }
+
+            }
+        }
+        return resultList;
+    }
+
+    // I used stack overflow and Khan Academy to figure this out.
+//Union
+
+    this.union = function (listA, listB) {
+
+        var resultList = [];
+
+        for (var i = 0; i < listA.length; i++) {  //set the element in listA (i) to start at 0, if it is less than the list length, continue.
+            var nextValue = listA[i]; // assign the listA item [i] to nextValue
+            var found = false; //if an item is found set it to false and continue to the next step
+
+            for (var j = 0; j < listB.length; j++) { // for every element in listB,  If the element is less than the list length continue
+
+                if (listB[j] === nextValue) { // if the listB item [j] is an equal value and type to nextValue[i], then it is true.
+                    found = true;
 
 
-        //get the symmetric difference of listA and ListB
-		//append the returned elements to the result set
-       for (var i = listA.length-1; i >= 0; --1)
-       	resultList [listA[i]] = listA[i];
-
-       for (var i = listB.length-1; i >= 0; -- i)
-       	obj[listB[i]] = listB[i];
-
-            var a = this.concat();
-            for(var i=0; i<a.length; ++i) {
-                for(var j=i+1; j<a.length; ++j) {
-                    if(a[i] === a[j])
-                        a.splice(j--, 1);
+                    break;//if listA if found in listB then break.
                 }
             }
+            if (found === false) {  //  if [j] was found true, set found to false and push to the result list.
 
-	   return resultList;  //return the result
-	}
+                resultList.push(nextValue);  // create a list and push the found items(nextValue) to the resultList at the end.
+            }
+
+        }
+        return resultList;  //return the results
+    }
+
 
 
 
 //Relative
 
-	this.relativeComplement = function(listA, listB) {
+    this.relativeComplement = function (listA, listB) {
 
-	   var resultList = [];
+        var resultList = [];
 
-        if (listA === null || listB === null) { /* check for valid inputs*/
-            return null; // exit and return null to indicate an error
 
-        //select everything in listA but not in listB  or  A \ B
-		// a - b = {}
-            for (var i = 0; i < listA.length; i++) {  //for every element in listA
-                var nextValue = listA[i]; // get next value in the list
+            for (var i = 0; i < listA.length; i++) {  //for every element in listA, If the element is less than the list length continue
+                var nextValue = listA[i]; // assign the listA item [i] to nextValue
+                var found = false; //if an item is found set it to false and continue to the next step
+
+                for (var j = 0; j < listB.length; j++) { // for every element in listB,  If the element is less than the list length continue
+
+                    if (listB[j] === nextValue) { // if the listB item [j] is an equal value and type to nextValue[i], then it is true.
+                        found = true;
+
+                        break;//if listA if found in listB then break.
+                    }
+
+
+                }
+                if (found === false) {  //  if [j] was found true, set found to false and push to the result list.
+
+                    resultList.push(nextValue);  // create a list and push the found items(nextValue) to the resultList at the end.
+                }
+
             }
-
-            for( var i = 0; i < listB.length; i++) {
-            break;}
+            return resultList;  //return the results
         }
-
-
-	   return resultList;
-	}
-
 
 
 //Symmetric
 
-	this.symmetricDifference = function(listA, listB) {
+    this.symmetricDifference = function (listA, listB) {
 
         var resultList = [];
 
-        for (var i = 0; i < listA.length; i++) {  //for every element in listA
-            var nextValue = listA[i]; // get next value in the list
+        for (var i = 0; i < listA.length; i++) {  //for every element in listA, If the element is less than the list length continue
+            var nextValue = listA[i]; // assign the listA item [i] to nextValue
+            var found = false; //if an item is found set it to false and continue to the next step
 
+            for (var j = 0; j < listB.length; j++) { // for every element in listB,  If the element is less than the list length continue
 
-            for (var j = 0; j < listB.length; j++) { // for every element in listB
+                if (listB[j] === nextValue) { // if the listB item [j] is an equal value and type to nextValue[i], then it is true.
+                    found = true;
 
-                if (listB[j] === nextValue) { // this listB element equals nextValue
                     break;//if listA if found in listB then break.
-
-					if (listB[j]!== nextValue) {
-                        resultList.push(listA[j]); // add listB element to end of resultList
-                                                   // push adds the current item in j to the end of the result list
-                        break;  // break the listB inner loop and exit
-
-                    }
-                    resultList = ListA.concat(listB);
                 }
+
+
+            }
+            if (found === false) {  //  if [j] was found true, set found to false and push to the result list.
+
+                resultList.push(nextValue);  // create a list and push the found items(nextValue) to the resultList at the end.
+            }
+
         }
-	   return resultList;  //return the results
-	}
+        return resultList;  //return the results
+    }
 
 
 }
